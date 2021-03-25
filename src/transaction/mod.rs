4ccb76d83f95fdcb0ea6a6ec8eeddc22a123d114
Manuel Mauro <manuel.mauro@protonmail.com>
2021-03-25 09:02:18 +0100

    Add optional fields to transaction header

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -75,9 +75,33 @@ pub struct Transaction {
     #[serde(rename = "gen", default)]
     pub genesis_id: String,
 
+    /// The group specifies that the transaction is part of a group and, if so, specifies the hash of
+    /// the transaction group. Assign a group ID to a transaction through the workflow described in
+    /// the Atomic Transfers Guide.
+    #[serde(rename = "grp", default)]
+    pub group: Option<HashDigest>,
+
+    /// A lease enforces mutual exclusion of transactions. If this field is nonzero, then once the
+    /// transaction is confirmed, it acquires the lease identified by the (Sender, Lease) pair of
+    /// the transaction until the LastValid round passes. While this transaction possesses the
+    /// lease, no other transaction specifying this lease can be confirmed. A lease is often used
+    /// in the context of Algorand Smart Contracts to prevent replay attacks. Read more about
+    /// Algorand Smart Contracts and see the Delegate Key Registration TEAL template for an example
+    /// implementation of leases. Leases can also be used to safeguard against unintended duplicate
+    /// spends. For example, if I send a transaction to the network and later realize my fee was too
+    /// low, I could send another transaction with a higher fee, but the same lease value. This would
+    /// ensure that only one of those transactions ends up getting confirmed during the validity period.
+    #[serde(rename = "lx", default)]
+    pub lease: Option<HashDigest>,
+
     /// Any data up to 1000 bytes.
     #[serde(with = "serde_bytes", default)]
     pub note: Vec<u8>,
+
+    /// Specifies the authorized address. This address will be used to authorize all future transactions.
+    /// Learn more about Rekeying accounts.
+    #[serde(rename = "rekey", default)]
+    pub rekey_to: Option<Address>,
 }
 
 impl Transaction {
@@ -95,7 +119,10 @@ impl Transaction {
             note: base.note,
             genesis_id: base.genesis_id,
             genesis_hash: base.genesis_hash,
+            group: None,
+            lease: None,
             txn_type,
+            rekey_to: None,
         };
         transaction.fee = MIN_TXN_FEE.max(fee_per_byte * transaction.estimate_size()?);
         Ok(transaction)
@@ -115,7 +142,10 @@ impl Transaction {
             note: base.note,
             genesis_id: base.genesis_id,
             genesis_hash: base.genesis_hash,
+            group: None,
+            lease: None,
             txn_type,
+            rekey_to: None,
         }
     }
 
