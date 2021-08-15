8c04e09ff11fe25ad6331212f969b691d9f5d06a
Ivan Schuetz <ivanschuetz@gmail.com>
2021-08-15 12:22:16 +0200

    Cleanup examples

--- examples/sign_offline.rs
+++ examples/sign_offline.rs
@@ -1,6 +1,5 @@
 use algonaut::algod::AlgodBuilder;
 use algonaut::core::{MicroAlgos, ToMsgPack};
-use algonaut::crypto::mnemonic;
 use algonaut::transaction::account::Account;
 use algonaut::transaction::{Pay, TxnBuilder};
 use dotenv::dotenv;
@@ -19,15 +18,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .auth(env::var("ALGOD_TOKEN")?.as_ref())
         .build_v2()?;
 
-    // print algod status
-    let node_status = algod.status().await?;
-    println!("node_status: {:?}", node_status);
-
-    let account = Account::generate();
-    println!("Public Key: {:?}", account.address().to_string());
-
-    let m = mnemonic::from_key(&account.seed())?;
-    println!("Backup phrase: {}", m);
+    let account = Account::from_mnemonic("auction inquiry lava second expand liberty glass involve ginger illness length room item discover ahead table doctor term tackle cement bonus profit right above catch")?;
 
     let params = algod.suggested_transaction_params().await?;
 
@@ -41,8 +32,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .build(),
     )
     .build();
-
-    println!("Made unsigned transaction: {:?}", t);
 
     // sign the transaction
     let signed_transaction = account.sign_transaction(&t)?;
