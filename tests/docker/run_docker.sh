b16032c59e7310e6bfcb71699376ee2f08b2259c
Manuel Mauro <manuel.mauro@protonmail.com>
2022-01-23 19:46:51 +0100

    bump rust version in docker test runner

--- tests/docker/run_docker.sh
+++ tests/docker/run_docker.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-
 set -e
 
 # reset test harness
 rm -rf test-harness
 rm -rf tests/features
+
 # fork with modified features, as cucumber-rs doesn't understand some syntax:
 # https://github.com/cucumber-rs/cucumber/issues/174
 # https://github.com/cucumber-rs/cucumber/issues/175
 # git clone --single-branch --branch master https://github.com/algorand/algorand-sdk-testing.git test-harness
 git clone --single-branch --branch master https://github.com/ivanschuetz/algorand-sdk-testing.git test-harness
+
 # copy feature files into project
 mv test-harness/features tests/features
 
-RUST_IMAGE=rust:1.57.0
+RUST_IMAGE=rust:1.58.1
 
 echo "Building docker image from base \"$RUST_IMAGE\""
 
