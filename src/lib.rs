cdb287b17f321219b50c74d5d5d210c773cddc17
Ivan Schuetz <ivanschuetz@gmail.com>
2022-02-19 15:17:34 +0100

    Update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,6 @@
 //! # algonaut
 //!
-//! Rust **algonaut** aims at becoming a rusty SDK for [Algorand](https://www.algorand.com/).
-//! Please, be aware that this crate is a work in progress.
-//!
-//! ## Objectives
-//!
-//! - Example-driven API development
-//! - Async requests
-//! - Builder pattern and sensible defaults
-//! - Modularity
-//! - Clear error messages
-//! - Thorough test suite
-//! - Comprehensive documentation
+//! Rust **algonaut** is a rusty SDK for [Algorand](https://www.algorand.com/). Please, be aware that this crate is a work in progress.
 
 // TODO #![deny(missing_docs)]
 
