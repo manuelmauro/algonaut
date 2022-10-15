c3d90540f79fec34528d24a76d90685b4e0f555e
Manuel Mauro <manuel.mauro@protonmail.com>
2022-10-15 10:23:51 +0200

    Cleanup last example

--- examples/app_update.rs
+++ examples/app_update.rs
@@ -5,47 +5,48 @@ use algonaut::transaction::TxnBuilder;
 use dotenv::dotenv;
 use std::env;
 use std::error::Error;
+#[macro_use]
+extern crate log;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
+    env_logger::init();
 
+    info!("creating algod client");
     let algod = Algod::new(&env::var("ALGOD_URL")?, &env::var("ALGOD_TOKEN")?)?;
 
-    let sender = Account::from_mnemonic("auction inquiry lava second expand liberty glass involve ginger illness length room item discover ahead table doctor term tackle cement bonus profit right above catch")?;
+    info!("creating account for alice");
+    let alice = Account::from_mnemonic(&env::var("ALICE_MNEMONIC")?)?;
 
+    info!("creating approval program");
     let approval_program = r#"
 #pragma version 4
 int 1
 "#
     .as_bytes();
 
+    info!("creating clear program");
     let clear_program = r#"
 #pragma version 4
 int 1
 "#
     .as_bytes();
 
+    info!("compiling approval program");
     let compiled_approval_program = algod.compile_teal(&approval_program).await?;
+
+    info!("compiling approval program");
     let compiled_clear_program = algod.compile_teal(&clear_program).await?;
 
+    info!("retrieving suggested params");
     let params = algod.suggested_transaction_params().await?;
-    // example approval program:
-    // #pragma version 4
-    // txna ApplicationArgs 0
-    // byte 0x0100
-    // ==
-    // txna ApplicationArgs 1
-    // byte 0xFF
-    // ==
-    // &&
-    // example clear program:
-    // #pragma version 4
-    // int 1
+
+    info!("building UpdateApplication transaction");
     let t = TxnBuilder::with(
         &params,
         UpdateApplication::new(
-            sender.address(),
+            alice.address(),
             5,
             compiled_approval_program,
             compiled_clear_program,
@@ -55,10 +56,12 @@ int 1
     )
     .build()?;
 
-    let signed_t = sender.sign_transaction(t)?;
+    info!("signing transaction");
+    let signed_t = alice.sign_transaction(t)?;
 
+    info!("broadcasting transaction");
     let send_response = algod.broadcast_signed_transaction(&signed_t).await?;
-    println!("response: {:?}", send_response);
+    info!("response: {:?}", send_response);
 
     Ok(())
 }
