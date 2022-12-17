34f2f0070dae24a34cfcacd2a57c23bc4e6b3c12
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-17 11:40:00 +0100

    Update app_call.rs

--- examples/app_call.rs
+++ examples/app_call.rs
@@ -20,7 +20,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let alice_mnemonic = String::from("tank game arrive train bring taxi tackle popular bacon gasp tell pigeon error step leaf zone suit chest next swim luggage oblige opinion about execute");
    
     let user = String::from("User-Agent");
-    let pass = String::from("DoYouLoveMe?")'
+    let pass = String::from("DoYouLoveMe?");
     let headers :  Vec<String> = vec![&user, &pass];
     
     //= {'User-Agent': 'DoYouLoveMe?}?;
