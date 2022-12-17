2f88f175bd2869f9065e2c4c1378290215ecfff1
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-17 11:23:06 +0100

    Update app_call.rs

--- examples/app_call.rs
+++ examples/app_call.rs
@@ -18,7 +18,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let token = String::from("");
     
     let alice_mnemonic = String::from("tank game arrive train bring taxi tackle popular bacon gasp tell pigeon error step leaf zone suit chest next swim luggage oblige opinion about execute");
-    let headers (&str) = ("User-Agent".to_string(), "DoYouLoveMe?".to_string());
+    let headers : (&str) = ("User-Agent".to_string(), "DoYouLoveMe?".to_string());
     
     //= {'User-Agent': 'DoYouLoveMe?}?;
     
