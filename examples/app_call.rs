16ac8bfdfe2a6e446aeb746a984eab949dc3e9bc
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-17 15:00:00 +0100

    Update app_call.rs

--- examples/app_call.rs
+++ examples/app_call.rs
@@ -45,7 +45,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let t = TxnBuilder::with(
         &params,
         CallApplication::new(alice.address(), app_id)
-            .app_arguments(vec![vec![0]])
+            .app_arguments(vec![vec![105, 110, 99]])
             .build(),
     )
     .build()?;
