dae3428d9bf9a60cacacce2127a3de25d713fec3
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-17 13:23:12 +0100

    Update app_call.rs

--- examples/app_call.rs
+++ examples/app_call.rs
@@ -37,15 +37,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("building transaction");
     let app_id : u64 = 116639568;
-    let app_arg = String::from("inc").as_bytes();
+    let app_arg : u8 = String::from("inc").as_bytes().parse();
 
-    let arg : u8 = app_arg;
-    println!(&app_arg);
+
+    println!("{}",&app_arg);
 
     let t = TxnBuilder::with(
         &params,
         CallApplication::new(alice.address(), app_id)
-            .app_arguments(vec![vec![arg]])
+            .app_arguments(vec![vec![app_arg]])
             .build(),
     )
     .build()?;
