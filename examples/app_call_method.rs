ad303544add7ba1704b931e1d9d71d0f0796f7c8
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2023-01-24 06:56:57 +0100

    Update app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -1,6 +1,8 @@
-use algonaut::atomic_transaction_composer::{AtomicTransactionComposer, AddMethodCallParams, transaction_signer::TransactionSigner}
+use algonaut::atomic_transaction_composer::{AtomicTransactionComposer, AddMethodCallParams, transaction_signer::TransactionSigner};
 
 
+use std::error::Error;
+#[macro_use]
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -10,9 +12,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
 AtomicTransactionComposer::add_method_call(  
  &self,
  &mut AddMethodCallParams
- )
-AtomicTransactionComposer::build_group()
+ );
+AtomicTransactionComposer::build_group();
  
-AtomicTransactionComposer::execute()
+AtomicTransactionComposer::execute();
  
 }
