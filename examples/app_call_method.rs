aec4bc6a1f6e1f39fc03c157ac04501759c11fde
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2023-01-24 07:08:44 +0100

    Update app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -1,5 +1,5 @@
 use algonaut::atomic_transaction_composer::{AtomicTransactionComposer, AddMethodCallParams, transaction_signer::TransactionSigner};
-
+use algonaut::algod::v2::Algod;
 
 use std::error::Error;
 #[macro_use]
@@ -7,14 +7,23 @@ use std::error::Error;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
  
+ let url = String::from("https://node.testnet.algoexplorerapi.io");
+ 
+ let user = String::from("User-Agent");
+ let pass = String::from("DoYouLoveMe?");
+ let headers :  Vec<(&str, &str)> = vec![(&user, &pass)];
 
-
+ let algod = Algod::with_headers(&url, headers)?;
+ 
+ 
 AtomicTransactionComposer::add_method_call(  
  &self,
  &mut AddMethodCallParams
  );
 AtomicTransactionComposer::build_group();
  
-AtomicTransactionComposer::execute();
+AtomicTransactionComposer::execute( &mut AtomicTransactionComposer ,&algod);;
+Ok(())
+ 
  
 }
