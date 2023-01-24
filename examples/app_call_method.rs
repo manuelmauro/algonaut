81d5dc4e4ec67faef050540f33c075129b6c709e
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2023-01-24 02:30:06 +0100

    Update app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -1,1 +1,29 @@
+use algonaut::atomic_transaction_composer::AtomicTransactionComposer
 
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+ 
+ let app_id: u64 = 01;
+ let method: AbiMethod = "";
+ let method_args: Vec<AbiArgValue> = "";
+ let fee: TxnFee = "";
+ let sender: Address = "";
+ let suggested_params: SuggestedTransactionParams = "";
+ let on_complete: ApplicationCallOnComplete ="";
+ let approval_program: Option<CompiledTeal> ="";
+ let clear_program: Option<CompiledTeal> ="";
+ let global_schema: Option<StateSchema> ="";
+ let local_schema: Option<StateSchema> =""; 
+ let extra_pages: u32 = 123;
+ let note: Option<Vec<u8>> ="";
+ let lease: Option<HashDigest> ="";
+ let rekey_to: Option<Address> ="";
+ let signer: TransactionSigner = "";
+
+AtomicTransactionComposer::add_method_call(
+   
+  )
+
+}
