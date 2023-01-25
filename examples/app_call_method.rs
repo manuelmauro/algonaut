450be2f1412bb92b3394b2b27d7a66a6f1e52477
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2023-01-25 11:27:20 +0100

    Update app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -61,16 +61,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
  let _note : Option<Vec<u8>> = Some(vec![0]);
  
  
-let mut AtomicTransactionComposer = AtomicTransactionComposer::add_method_call( &mut AtomicTransactionComposer , &mut AddMethodCallParams{
+let mut ATC = AtomicTransactionComposer::add_method_call( &mut AtomicTransactionComposer , &mut AddMethodCallParams{
     app_id: 155672004, method: _method, method_args: [arg1, arg2], fee:  Fixed(MicroAlgos(2500)), sender: acct1.address(), suggested_params: params, on_complete: NoOp,
     approval_program: None, clear_program: None, global_schema: None, local_schema: None, extra_pages: pages, 
     note: _note, lease: None, rekey_to: None, signer: BasicAccount(acct1)
     });
     
 //println!("{}",&mut AtomicTransactionComposer);
-AtomicTransactionComposer::build_group(&mut AtomicTransactionComposer.unwrap());
+AtomicTransactionComposer::build_group(&mut ATC);
  
-AtomicTransactionComposer::execute( &mut AtomicTransactionComposer ,&algod);
+AtomicTransactionComposer::execute( &mut ATC ,&algod);
 Ok(())
  
  
