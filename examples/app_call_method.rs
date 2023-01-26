297365071119c07c6f3d602eccd986ed10a6a392
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2023-01-26 11:59:05 +0100

    Update app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -76,7 +76,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 
 let mut ATC2 = AtomicTransactionComposer::add_method_call(
-          AddMethodCallParams: AddMethodCallParams {
+          AddMethodCallParams {
                  app_id: 155672004,
                  method: _method,
                  method_args: vec![arg1, arg2],
