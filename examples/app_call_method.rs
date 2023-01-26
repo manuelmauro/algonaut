cbca34159d3811b957363d05a4daaf70eae3b983
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2023-01-26 14:46:50 +0100

    Update app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -83,9 +83,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 
 
- _hashmap.insert(q,q);
- _hashmap.insert(_method2,_method2); 
  
+ _hashmap.insert(_method2,_method2); 
+ _hashmap.insert(q,q);
+    
  const _note : Option<Vec<u8>> = Some(vec![0]);
 //println!("building Pay transaction");
  let t = TxnBuilder::with(
