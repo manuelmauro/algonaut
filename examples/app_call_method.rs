6d3811aefdfbeafe148238c5f7150a8422e4b4c7
sam2much96 <sam2much96@gmail.com>
2023-03-02 22:08:08 +0100

    modified:   examples/app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -264,7 +264,7 @@ let t2 = t.unwrap().clone();
 
 //let sign_txn = acct1.sign_transaction(t2)?;
 
-let _app_id = 161734720;
+let _app_id = 161736780;
 
 //let _Escrow_Address : Address = logic.get_application_address(_app_id);
 
