eb9b5ca156567ef26fccc2f3e383f49f73860721
sam2much96 <sam2much96@gmail.com>
2023-03-05 12:09:03 +0100

    modified:   examples/app_call_method.rs

--- examples/app_call_method.rs
+++ examples/app_call_method.rs
@@ -4,8 +4,8 @@
   
  
 mod bar {
-    use algonaut_abi::abi_interactions::AbiMethodArg;
-    use algonaut_abi::abi_interactions::AbiReturn;
+    //use algonaut_abi::abi_interactions::AbiMethodArg;
+    //use algonaut_abi::abi_interactions::AbiReturn;
     use algonaut_abi::abi_type::AbiType;
 
 
@@ -20,8 +20,8 @@ mod bar {
 
     impl MyTrait for Foo {
         type Foo = Foo;
-        type type_ = String;
-        type parsed = Option<String>;
+        type Type = String;
+        type Parsed = Option<String>;
 
         fn new() -> Self::Foo {
             Foo {
@@ -32,17 +32,17 @@ mod bar {
             }
         }
 
-        fn type_() -> String { "".to_string() }
+        fn r#type() -> String { "".to_string() }
         fn parsed() -> Option<AbiType> { None }
     }
 
     trait MyTrait {
         type Foo;
-        type type_: ToString;
-        type parsed;
+        type Type: ToString;
+        type Parsed;
 
         fn new() -> Self::Foo;
-        fn type_() -> String;
+        fn r#type() -> String;
         fn parsed() -> Option<AbiType>;
     }
 
@@ -94,12 +94,12 @@ mod bar {
 //#[derive(Deref, DerefMut, From)]
 mod params {
 
-use algonaut_core::SuggestedTransactionParams;
-use algonaut_core::MicroAlgos;
-use algonaut_core::Round;
-use algonaut_crypto::HashDigest;
-use std::collections::HashMap; //inplace of dictionary crate
-use core::fmt::Error;
+//use algonaut_core::SuggestedTransactionParams;
+//use algonaut_core::MicroAlgos;
+//use algonaut_core::Round;
+//use algonaut_crypto::HashDigest;
+//use std::collections::HashMap; //inplace of dictionary crate
+//use core::fmt::Error;
 
 
 use rmp_serde::from_slice;
@@ -107,9 +107,10 @@ pub mod ATC {
     /*
     Atomic Transaction Composer Required Traits
     */
-    use algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus as OtherAtomicTransactionComposerStatus;
+    //use algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus as OtherAtomicTransactionComposerStatus;
     use std::string::String as str;
-    use algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus::Building;
+    //use algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus::Building;
+    //use algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus;
 
     pub enum AtomicTransactionComposerStatus {
         Building,
@@ -119,42 +120,34 @@ pub mod ATC {
         Committed,
     }
 
-    trait Into {
-        type Into ;//: 'static;
-        //type Into: <&'a str> ;//: 'static;
-        type From; 
-        type T ; //: <&str>;
-        
-        //type <'a> ;
-        //type <T,T>;
-        //type From<AtomicTransactionComposerStatus>;
-        //type From<&mut AtomicTransactionComposerStatus>;
-        fn into <T: From<T> + ?Sized>(b : &T) {
-            todo!()
+    impl std::fmt::Display for AtomicTransactionComposerStatus {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                AtomicTransactionComposerStatus::Building => write!(f, "Building"),
+                AtomicTransactionComposerStatus::Built => write!(f, "Built"),
+                AtomicTransactionComposerStatus::Signed => write!(f, "Signed"),
+                AtomicTransactionComposerStatus::Submitted => write!(f, "Submitted"),
+                AtomicTransactionComposerStatus::Committed => write!(f, "Committed"),
+            }
         }
     }
 
-    // Implement the trait for AtomicTransactionComposerStatus
-    //impl Into for dyn From<T>  {
-    //    type Into = &'static str;
+    pub trait Into {
+        type Into;
+        type From;
+        type T;
 
+        fn into<T: From<Self::T> + ?Sized>(_b: &T) {
+            todo!()
+        }
 
-    //    fn into <T: From<T> + ?Sized>(b : &T) {
-    //        todo!()
-    //    }
-    //}
-
-    // Implement the Into trait for &'a AtomicTransactionComposerStatus to &'a str
-    //impl<'a> Into<&'a str> for &'a AtomicTransactionComposerStatus {
-    //    fn into(self) -> &'a str {
-    //        (&*self).into()
-    //    }
-    //}
+        fn into_boxed_str() -> &'static str;
+    }
 
     // Implement the From trait for AtomicTransactionComposerStatus to &'static str
     impl<'a> From<AtomicTransactionComposerStatus> for &'a str {
-        fn from(_: AtomicTransactionComposerStatus) -> &'a str {
-            todo!()
+        fn from(s: AtomicTransactionComposerStatus) -> &'a str {
+            Box::leak(Box::new(s.to_string()))
         }
     }
 
@@ -164,6 +157,7 @@ pub mod ATC {
             todo!()
         }
     }
+}
 
      // Implement the From trait for String to &'static str
     //impl From<str> for dyn T  {
@@ -190,17 +184,17 @@ pub mod ATC {
     //        todo!()
     //    }
     //}
-}
+//}
 
 
 pub mod params {
     use algonaut_core::SuggestedTransactionParams;
-    use std::collections::HashMap;
-    use crate::params::from_slice;
+    //use std::collections::HashMap;
+    //use crate::params::from_slice;
     use algonaut_model::algod::v2::TransactionParams;
     use algonaut_core::MicroAlgos;
     use algonaut_core::Round;
-    use algonaut_crypto::HashDigest;
+    //use algonaut_crypto::HashDigest;
     //use crate::params::SuggestedTransactionParams;
 
     pub struct MySuggestedTransactionParams(SuggestedTransactionParams);
@@ -285,20 +279,20 @@ pub mod escrow {
 
     use algonaut::{
         atomic_transaction_composer::{
-            transaction_signer::TransactionSigner, AbiArgValue, AbiMethodReturnValue,
-            AbiReturnDecodeError, AddMethodCallParams, AtomicTransactionComposer,
-            AtomicTransactionComposerStatus, TransactionWithSigner,
+            transaction_signer::TransactionSigner, AbiArgValue, //AbiMethodReturnValue,
+            AtomicTransactionComposer, //AbiReturnDecodeError, AddMethodCallParams, 
+            TransactionWithSigner, //AtomicTransactionComposerStatus, 
         },
         error::ServiceError,
     };
-    use algonaut_abi::{
-        abi_interactions::{AbiArgType, AbiMethod, AbiReturn, AbiReturnType, ReferenceArgType},
-        abi_type::{AbiType, AbiValue as OtherAbiValue},
-    };
-    use algonaut_core::{to_app_address, Address as OtherAddress, MicroAlgos, CompiledTeal};
-    use algonaut_model::algod::v2::PendingTransaction;
+    //use algonaut_abi::{
+    //    abi_interactions::{AbiArgType, AbiMethod, AbiReturn, AbiReturnType, ReferenceArgType},
+    //    abi_type::{AbiType, AbiValue as OtherAbiValue},
+    //};
+    use algonaut_core::{to_app_address, Address as OtherAddress, MicroAlgos};//, CompiledTeal};
+    //use algonaut_model::algod::v2::PendingTransaction;
     use algonaut_transaction::{
-        builder::TxnFee,
+        //builder::TxnFee,
         transaction::{ApplicationCallOnComplete, StateSchema},
         Pay, TxnBuilder,
     };
@@ -308,18 +302,19 @@ pub mod escrow {
 
     //combine both
     use algonaut_transaction::account::Account;
-    use algonaut_transaction::Transaction;
+    //use algonaut_transaction::Transaction;
 
-    use algonaut_crypto::HashDigest;
-    use algonaut_core::Round;
+    //use algonaut_crypto::HashDigest;
+    //use algonaut_core::Round;
     //use algonaut_core::SuggestedTransactionParams as OtherSuggestedTransactionParams;
 
+    use std::convert::TryInto;
     //my cyustom params
     use crate::params::params::MySuggestedTransactionParams;
     
     //lifetime Parameter
     pub struct Foo <'a> {
-        pub withdrw_amt: BigUint,
+        pub withdrw_amt: u32,
         pub withdrw_to_addr: [u8; 32],
         pub arg1: AbiArgValue,
         pub arg2: AbiArgValue,
@@ -376,9 +371,28 @@ pub mod escrow {
             //x
         }
         
-    
+        pub fn note(size : u32) -> Option <Vec<u8>>{
+            Some(vec![size.try_into().unwrap()])
+
+
+        }
     
 
+
+        pub fn withdraw_amount(amount : u32) -> AbiArgValue {
+            /*
+            Converts a U64 int to Big Uint and returns an AbiArg Value
+            
+            */
+            let withdrw_amt : BigUint = BigUint::new(vec![amount]); //in MicroAlgos
+            
+
+            let arg1 : AbiArgValue = AbiArgValue::AbiValue( Int(withdrw_amt));
+            arg1
+
+
+        }
+            
     
         
         pub fn withdraw(acct1: Account ){
@@ -390,7 +404,7 @@ pub mod escrow {
             
             */
 
-            let withdrw_amt : BigUint = BigUint::new(vec![0]); //in MicroAlgos
+            //let withdrw_amt : BigUint = BigUint::new(vec![0]); //in MicroAlgos
             
             // Address As Bytes
             let mut withdrw_to_addr: [u8; 32] = [0; 32];
@@ -400,12 +414,15 @@ pub mod escrow {
             withdrw_to_addr.copy_from_slice(&acct1.address().to_string().as_bytes()[..32]);
 
 
-            //App Call Arguments For AtomicTransactioComposer
+            //App Call Arguments For AtomicTransactionComposer
             // Withdraw Method
-            let arg1 : AbiArgValue = AbiArgValue::AbiValue( Int(withdrw_amt));
+            // Unused in the Current Scop
+            
+            //let arg1 : AbiArgValue = AbiArgValue::AbiValue( Int(withdrw_amt));
             
             let arg2: AbiArgValue = AbiArgValue::AbiValue(algonaut_abi::abi_type::AbiValue::Address(OtherAddress::new(withdrw_to_addr)));
         }
+        
 
         //use algonaut_core::Address;
         pub fn pay(to_address : algonaut_core::Address , acct1 : Account, _params : algonaut_core::SuggestedTransactionParams) -> algonaut_transaction::Transaction{
@@ -480,24 +497,24 @@ use algonaut::transaction::{account::Account,
 };
 use algonaut_transaction::builder::TxnFee::Fixed;
 
-use algonaut_abi::abi_interactions::AbiReturnType::Void;
+//use algonaut_abi::abi_interactions::AbiReturnType::Void;
 
 
-use algonaut::atomic_transaction_composer::{ AbiArgValue::AbiValue};
+//use algonaut::atomic_transaction_composer::{ AbiArgValue::AbiValue};
 
-use algonaut_abi::abi_interactions::{AbiMethodArg};
+//use algonaut_abi::abi_interactions::{AbiMethodArg};
 use num_bigint::BigUint;
 use algonaut_abi::abi_type::AbiType::Address;
 use algonaut_abi::abi_type::AbiValue::Int;
 
 
-use algonaut_crypto::HashDigest;
+//use algonaut_crypto::HashDigest;
 use algonaut::algod::v2::Algod;
 
 
-use algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus::Building;
+//use algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus::Building;
 
-use std::collections::HashMap;
+//use std::collections::HashMap;
 
 
 use algonaut::{
@@ -506,7 +523,7 @@ use algonaut::{
         AbiReturnDecodeError, AddMethodCallParams, AtomicTransactionComposer,
         AtomicTransactionComposerStatus, TransactionWithSigner,
     },
-    error::ServiceError,
+    //error::ServiceError,
 };
 use algonaut_abi::{
     abi_interactions::{AbiArgType, AbiMethod, AbiReturn, AbiReturnType, ReferenceArgType},
@@ -519,6 +536,47 @@ use algonaut_transaction::{
     transaction::{ApplicationCallOnComplete, StateSchema},
     Pay, TxnBuilder,
 };
+
+
+
+//GOdot FUnction Template
+//#[method]
+//#[allow(clippy::too_many_arguments)]
+    
+//    fn construct_app_call_method(
+//        &self,
+//        #[base] _base: &Node,
+//        app_id: u64,
+//        method: AbiMethod,
+//        method_args: Vec<AbiArgValue>,
+//        fee: TxnFee,
+//        sender: Address,
+//        on_complete: ApplicationCallOnComplete,
+//        clear_program: Option<CompiledTeal>,
+//        global_schema: Option<StateSchema>,
+//        local_schema: Option<StateSchema>,
+//        extra_pages: u32,
+//        note: Option<Vec<u8>>,
+//        lease: Option<HashDigest>,
+//        rekey_to: Option<Address>,
+//        signer: TransactionSigner,
+    
+    
+//    ) -> Transaction {
+        
+//        TxnBuilder::with(
+//       )
+//        .build()
+//        .unwrap()
+ //       .into()
+    
+    
+  //  }
+    
+
+
+
+
 //use cucumber::{codegen::Regex, given, then, when};
 use data_encoding::BASE64;
 use num_traits::ToPrimitive;
@@ -533,6 +591,8 @@ use crate::params::ATC;
 #[macro_use]
 
 #[tokio::main]
+
+
 
 async fn main() -> Result<(), Box<dyn Error>> {
 
@@ -587,8 +647,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
  
  //_hashmap.insert(Q,_deposit.clone()); 
     
- let _note : Option<Vec<u8>> = Some(vec![0]);
-
 
 
 
@@ -609,22 +667,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     //Txn Details As a Struct
     let details = escrow::Foo { 
-            withdrw_amt : BigUint::new(vec![0]),//BigUint { data: vec![0u64] },//BigUint = BigUint::new(vec![0]), 
+            withdrw_amt : 0u32,//Foo::withdraw_amount(0u32),//BigUint::new(vec![0]),//BigUint { data: vec![0u64] },//BigUint = BigUint::new(vec![0]), 
             withdrw_to_addr: _to_addr.clone(), 
-            arg1: AbiArgValue::AbiValue( Int(0u64.into())), 
+            arg1: Foo::withdraw_amount(0u32),//AbiArgValue::AbiValue( Int(0u64.into())), 
             arg2: AbiArgValue::AbiValue(algonaut_abi::abi_type::AbiValue::Address(OtherAddress::new(_to_addr))), 
             _app_id: 161737986, 
             _escrow_address: to_app_address(161737986), 
             atc: &atc };
 
-    let q = details.arg1;
+    //let q = details.arg1;
 
-    let p = details.arg2;
+    //let p = details.arg2;
     //Add method Call     
     atc.add_method_call( &mut AddMethodCallParams {
-                    app_id: 161737986,//escrow::Foo::_app_id,
+                    app_id: details._app_id,//161737986,//escrow::Foo::_app_id,
                     method: _withdraw,
-                    method_args: vec![q, p],
+                    method_args: vec![details.arg1, details.arg2],
                     fee: Fixed(MicroAlgos(2500)),
                     sender: acct1_2.address(),
                     suggested_params: params,
@@ -634,7 +692,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     global_schema: None,
                     local_schema: None,
                     extra_pages: pages,
-                    note: _note,
+                    note: Foo::note(0u32),//_note,
                     lease: None,
                     rekey_to: None,
                     signer: BasicAccount(acct1_2)
@@ -649,6 +707,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
    
  let status_str : &mut AtomicTransactionComposerStatus = &mut atc.status();
 
+ //ATC::Into(status_str);
  //Buggy Trait Implementation
  //let t : _= <&mut algonaut::atomic_transaction_composer::AtomicTransactionComposerStatus as std::convert::Into<T>>::into(status_str);  // {
  
