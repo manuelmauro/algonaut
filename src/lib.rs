3292894501d99fcb8a9502ebf042cdb493bfa0b7
Manuel Mauro <manuel.mauro@protonmail.com>
2023-04-22 20:04:47 +0200

    Re-export new crates

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,22 @@
 // Re-exports
 
 pub use algonaut_abi as abi;
-pub use algonaut_algod as openapi_algod;
-
 pub use algonaut_core as core;
 pub use algonaut_crypto as crypto;
 pub use algonaut_model as model;
 pub use algonaut_transaction as transaction;
 
+pub use algonaut_algod as openapi_algod;
+pub use algonaut_indexer as openapi_indexer;
+pub use algonaut_kmd as openapi_kmd;
+
 pub mod algod;
+pub mod indexer;
+pub mod kmd;
+
 pub mod atomic_transaction_composer;
+
 pub mod error;
 pub use error::Error;
 
-pub mod indexer;
-pub mod kmd;
 pub mod util;
